da9f9d78d0f500e7e06e93cf39fab590ac521820
Robert Escriva <robert@rescrv.net>
2016-12-12 06:32:42 -0800

    Adjust license to 3-clause BSD
    Signed-off-by: Robert Escriva <robert@rescrv.net>

--- txman/local_voter.cc
+++ txman/local_voter.cc
@@ -1,6 +1,29 @@
-// w
-// Copyright (c) 2015, Robert Escriva
+// Copyright (c) 2015-2016, Robert Escriva, Cornell University
 // All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Consus nor the names of its contributors may be
+//       used to endorse or promote products derived from this software without
+//       specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 // Google Log
 #include <glog/logging.h>
@@ -277,7 +300,7 @@ local_voter :: vote_learn(unsigned idx, uint64_t v, daemon* d)
 
     bool log = false;
 
-    if (m_votes[idx].phase() == paxos_synod::LEARNED && 
+    if (m_votes[idx].phase() == paxos_synod::LEARNED &&
         m_votes[idx].learned() != v)
     {
         // this should never happen; let's catch if it does so we can make sure
